c215866116a438e4796b4fd3a9ecab22ee284f5d
Chi Ho <>
2020-05-28 02:07:38 +0200

    add search bar components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import RandomMeals from './components/randomMeals';
 import ListRandomMeals from './components/listRandomMeals';
+import SearchBar from './components/searchBar';
+import SearchResult from './components/searchResult';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      <SearchBar/>
+      <SearchResult/>
       <RandomMeals/>
       <ListRandomMeals/>
     </div>
