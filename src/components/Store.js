f59417072d349fad3d8fc2bfc0d7e8908c462e1a
Chi Ho <>
2020-05-31 01:19:03 +0200

    reusable store component

---
+++ src/components/Store.js
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware } from 'redux';
+import getData from '../reducers';
+import thunk from 'redux-thunk';
+
+export const store = createStore(
+    getData,
+    applyMiddleware(thunk)
+);
