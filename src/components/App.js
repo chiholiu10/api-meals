c9a76e8f264f4a43c339c812ba1825ee509a1ddf
Chi Ho <>
2020-05-31 21:48:48 +0200

    AppComponent modified name

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { RandomMeals } from './RandomMeals';
-import ListRandomMeals from './ListRandomMeals';
+import {ListRandomMeals } from './ListRandomMeals';
 import { SearchBar } from './SearchBar';
 import SearchResult from './SearchResult';
 
-export const App = () => (
+export const AppComponent = () => (
   <div>
     <RandomMeals/>
     <SearchBar/>
