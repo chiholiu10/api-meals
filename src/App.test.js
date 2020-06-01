0ed8a6a205bcc15106cd33648238a9ef9f267a0e
Chi Ho <>
2020-06-01 23:25:32 +0200

    app test code

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,10 @@ import { render } from '@testing-library/react';
 import AppComponent from './App';
 import { Provider } from 'react-redux';
 import { store } from './Store';
+import { screen } from "@testing-library/dom";
 
 test('App not crashing', () => {
-  const appComponent = render(<Provider store={store}><AppComponent /></Provider>);
-  const element = appComponent.getByTestId('test-app-component');
+  render(<Provider store={store}><AppComponent /></Provider>);
+  const element = screen.getAllByTestId('app-component');
+  expect(element).toHaveLength(1)
 });
