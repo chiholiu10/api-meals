bb32a6ffdcc345ef3204e004212a9278a2a8b083
Chi Ho <>
2020-06-01 11:42:53 +0200

    adding classnames and styling

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,15 @@ import { SearchBar } from './SearchBar';
 import SearchResult from './SearchResult';
 
 export const AppComponent = () => (
-  <div>
-    <RandomMeals/>
-    <SearchBar/>
-    <SearchResult/>
-    <ListRandomMeals/>
+  <div className="app-container">
+    <div className="app-mobile app-desktop-left">
+      <RandomMeals/>
+      <SearchBar/>
+      <SearchResult/>
+    </div>
+    <div className="app-mobile app-desktop-right">
+      <ListRandomMeals/>
+    </div>
+
   </div>
 )
