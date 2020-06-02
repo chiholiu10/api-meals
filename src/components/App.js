de9035946825b94d713380f23272a95b48942d51
Chi Ho <>
2020-06-02 14:59:27 +0200

    write test top bar component

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,22 @@ import React from 'react'
 import { RandomMeals } from './RandomMeals/RandomMeals';
 import ListRandomMeals from './ListRandomMeals/ListRandomMeals';
 import { SearchBar } from './SearchBar/SearchBar';
+import { TopBar } from './TopBar/TopBar';
 import SearchResult from './SearchResult/SearchResult';
 
 export const AppComponent = () => (
   <div className="app-container">
-    <div className="app-mobile app-desktop-left">
-      <RandomMeals/>
-      <SearchBar/>
-      <SearchResult/>
-    </div>
-    <div className="app-mobile app-desktop-right">
-      <ListRandomMeals/>
-    </div>
+
+      <TopBar/>
+      
+      <div className="app-mobile app-desktop-left">
+        <RandomMeals/>
+        <SearchBar/>
+        <SearchResult/>
+      </div>
+
+      <div className="app-mobile app-desktop-right">
+        <ListRandomMeals/>
+      </div>
   </div>
 )
